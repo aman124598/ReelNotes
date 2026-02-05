9e5874ff78c448fb5b9168e88e867d77d2476c8c
Aman <amanraj89969@gmail.com>
2026-02-05 16:28:21 +0530

    bug fix

--- app/App.js
+++ app/App.js
@@ -1,7 +1,9 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { NoteDetailScreen } from './src/screens/NoteDetailScreen';
 import { AddNoteScreen } from './src/screens/AddNoteScreen';
@@ -10,18 +12,20 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="light" />
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyle: { backgroundColor: '#0A0A0A' },
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="NoteDetail" component={NoteDetailScreen} />
-        <Stack.Screen name="AddNote" component={AddNoteScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            cardStyle: { backgroundColor: '#0A0A0A' },
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="NoteDetail" component={NoteDetailScreen} />
+          <Stack.Screen name="AddNote" component={AddNoteScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
